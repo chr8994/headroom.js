8835e128c7608863f30a3ac38c8646e0f0f2ad15
Christian Gomez <chr8993@gmail.com>
2017-09-22 16:46:04 -0500

    Fix

--- dist/headroom.js
+++ dist/headroom.js
@@ -23,14 +23,14 @@
   'use strict';
 
   /* exported features */
-  
+
   var features = {
     bind : !!(function(){}.bind),
     classList : 'classList' in document.documentElement,
     rAF : !!(window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame)
   };
   window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
-  
+
   /**
    * Handles debouncing of events via requestAnimationFrame
    * @see http://www.html5rocks.com/en/tutorials/speed/animations/
@@ -42,7 +42,7 @@
   }
   Debouncer.prototype = {
     constructor : Debouncer,
-  
+
     /**
      * dispatches the event to the supplied callback
      * @private
@@ -51,7 +51,7 @@
       this.callback && this.callback();
       this.ticking = false;
     },
-  
+
     /**
      * ensures events don't get stacked
      * @private
@@ -62,7 +62,7 @@
         this.ticking = true;
       }
     },
-  
+
     /**
      * Attach this as the event listeners
      */
@@ -78,7 +78,7 @@
   function isDOMElement(obj) {
     return obj && typeof window !== 'undefined' && (obj === window || obj.nodeType);
   }
-  
+
   /**
    * Helper function for extending objects
    */
@@ -86,14 +86,14 @@
     if(arguments.length <= 0) {
       throw new Error('Missing arguments in extend function');
     }
-  
+
     var result = object || {},
         key,
         i;
-  
+
     for (i = 1; i < arguments.length; i++) {
       var replacement = arguments[i] || {};
-  
+
       for (key in replacement) {
         // Recurse into object except if the object is a DOM element
         if(typeof result[key] === 'object' && ! isDOMElement(result[key])) {
@@ -104,17 +104,17 @@
         }
       }
     }
-  
+
     return result;
   }
-  
+
   /**
    * Helper function for normalizing tolerance option to object format
    */
   function normalizeTolerance (t) {
     return t === Object(t) ? t : { down : t, up : t };
   }
-  
+
   /**
    * UI enhancement for fixed headers.
    * Hides header when scrolling down
@@ -125,7 +125,7 @@
    */
   function Headroom (elem, options) {
     options = extend(options, Headroom.options);
-  
+
     this.lastKnownScrollY = 0;
     this.elem             = elem;
     this.tolerance        = normalizeTolerance(options.tolerance);
@@ -142,7 +142,7 @@
   }
   Headroom.prototype = {
     constructor : Headroom,
-  
+
     /**
      * Initialises the widget
      */
@@ -150,34 +150,34 @@
       if(!Headroom.cutsTheMustard) {
         return;
       }
-  
+
       this.debouncer = new Debouncer(this.update.bind(this));
       this.elem.classList.add(this.classes.initial);
-  
+
       // defer event registration to handle browser
       // potentially restoring previous scroll position
       setTimeout(this.attachEvent.bind(this), 100);
-  
+
       return this;
     },
-  
+
     /**
      * Unattaches events and removes any classes that were added
      */
     destroy : function() {
       var classes = this.classes;
-  
+
       this.initialised = false;
-  
+
       for (var key in classes) {
         if(classes.hasOwnProperty(key)) {
           this.elem.classList.remove(classes[key]);
         }
       }
-  
+
       this.scroller.removeEventListener('scroll', this.debouncer, false);
     },
-  
+
     /**
      * Attaches the scroll event
      * @private
@@ -187,92 +187,92 @@
         this.lastKnownScrollY = this.getScrollY();
         this.initialised = true;
         this.scroller.addEventListener('scroll', this.debouncer, false);
-  
+
         this.debouncer.handleEvent();
       }
     },
-  
+
     /**
      * Unpins the header if it's currently pinned
      */
     unpin : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(classList.contains(classes.pinned) || !classList.contains(classes.unpinned)) {
         classList.add(classes.unpinned);
         classList.remove(classes.pinned);
         this.onUnpin && this.onUnpin.call(this);
       }
     },
-  
+
     /**
      * Pins the header if it's currently unpinned
      */
     pin : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(classList.contains(classes.unpinned)) {
         classList.remove(classes.unpinned);
         classList.add(classes.pinned);
         this.onPin && this.onPin.call(this);
       }
     },
-  
+
     /**
      * Handles the top states
      */
     top : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(!classList.contains(classes.top)) {
         classList.add(classes.top);
         classList.remove(classes.notTop);
         this.onTop && this.onTop.call(this);
       }
     },
-  
+
     /**
      * Handles the not top state
      */
     notTop : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(!classList.contains(classes.notTop)) {
         classList.add(classes.notTop);
         classList.remove(classes.top);
         this.onNotTop && this.onNotTop.call(this);
       }
     },
-  
+
     bottom : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(!classList.contains(classes.bottom)) {
         classList.add(classes.bottom);
         classList.remove(classes.notBottom);
         this.onBottom && this.onBottom.call(this);
       }
     },
-  
+
     /**
      * Handles the not top state
      */
     notBottom : function() {
       var classList = this.elem.classList,
         classes = this.classes;
-  
+
       if(!classList.contains(classes.notBottom)) {
         classList.add(classes.notBottom);
         classList.remove(classes.bottom);
         this.onNotBottom && this.onNotBottom.call(this);
       }
     },
-  
+
     /**
      * Gets the Y scroll position
      * @see https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY
@@ -285,7 +285,7 @@
           ? this.scroller.scrollTop
           : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     },
-  
+
     /**
      * Gets the height of the viewport
      * @see http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript
@@ -296,7 +296,7 @@
         || document.documentElement.clientHeight
         || document.body.clientHeight;
     },
-  
+
     /**
      * Gets the physical height of the DOM element
      * @param  {Object}  elm the element to calculate the physical height of which
@@ -308,7 +308,7 @@
         elm.clientHeight
       );
     },
-  
+
     /**
      * Gets the physical height of the scroller element
      * @return {int} the physical height of the scroller element in pixels
@@ -318,7 +318,7 @@
         ? this.getViewportHeight()
         : this.getElementPhysicalHeight(this.scroller);
     },
-  
+
     /**
      * Gets the height of the document
      * @see http://james.padolsey.com/javascript/get-document-height-cross-browser/
@@ -327,14 +327,14 @@
     getDocumentHeight : function () {
       var body = document.body,
         documentElement = document.documentElement;
-  
+
       return Math.max(
         body.scrollHeight, documentElement.scrollHeight,
         body.offsetHeight, documentElement.offsetHeight,
         body.clientHeight, documentElement.clientHeight
       );
     },
-  
+
     /**
      * Gets the height of the DOM element
      * @param  {Object}  elm the element to calculate the height of which
@@ -347,7 +347,7 @@
         elm.clientHeight
       );
     },
-  
+
     /**
      * Gets the height of the scroller element
      * @return {int} the height of the scroller element in pixels
@@ -357,7 +357,7 @@
         ? this.getDocumentHeight()
         : this.getElementHeight(this.scroller);
     },
-  
+
     /**
      * determines if the scroll position is outside of document boundaries
      * @param  {int}  currentScrollY the current y scroll position
@@ -366,10 +366,10 @@
     isOutOfBounds : function (currentScrollY) {
       var pastTop  = currentScrollY < 0,
         pastBottom = currentScrollY + this.getScrollerPhysicalHeight() > this.getScrollerHeight();
-  
+
       return pastTop || pastBottom;
     },
-  
+
     /**
      * determines if the tolerance has been exceeded
      * @param  {int} currentScrollY the current scroll y position
@@ -378,7 +378,7 @@
     toleranceExceeded : function (currentScrollY, direction) {
       return Math.abs(currentScrollY-this.lastKnownScrollY) >= this.tolerance[direction];
     },
-  
+
     /**
      * determine if it is appropriate to unpin
      * @param  {int} currentScrollY the current y scroll position
@@ -388,10 +388,10 @@
     shouldUnpin : function (currentScrollY, toleranceExceeded) {
       var scrollingDown = currentScrollY > this.lastKnownScrollY,
         pastOffset = currentScrollY >= this.offset;
-  
+
       return scrollingDown && pastOffset && toleranceExceeded;
     },
-  
+
     /**
      * determine if it is appropriate to pin
      * @param  {int} currentScrollY the current y scroll position
@@ -401,10 +401,10 @@
     shouldPin : function (currentScrollY, toleranceExceeded) {
       var scrollingUp  = currentScrollY < this.lastKnownScrollY,
         pastOffset = currentScrollY <= this.offset;
-  
+
       return (scrollingUp && toleranceExceeded) || pastOffset;
     },
-  
+
     /**
      * Handles updating the state of the widget
      */
@@ -412,31 +412,31 @@
       var currentScrollY  = this.getScrollY(),
         scrollDirection = currentScrollY > this.lastKnownScrollY ? 'down' : 'up',
         toleranceExceeded = this.toleranceExceeded(currentScrollY, scrollDirection);
-  
+
       if(this.isOutOfBounds(currentScrollY)) { // Ignore bouncy scrolling in OSX
         return;
       }
-  
+
       if (currentScrollY <= this.offset ) {
         this.top();
       } else {
         this.notTop();
       }
-  
+
       if(currentScrollY + this.getViewportHeight() >= this.getScrollerHeight()) {
         this.bottom();
       }
       else {
         this.notBottom();
       }
-  
+
       if(this.shouldUnpin(currentScrollY, toleranceExceeded)) {
         this.unpin();
       }
       else if(this.shouldPin(currentScrollY, toleranceExceeded)) {
         this.pin();
       }
-  
+
       this.lastKnownScrollY = currentScrollY;
     }
   };
@@ -465,3 +465,97 @@
 
   return Headroom;
 }));
+
+/*!
+ * headroom.js v0.9.4 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2017 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * License: MIT
+ */
+
+(function (angular, Headroom) {
+
+  if(!angular) {
+    return;
+  }
+
+  function headroom(HeadroomService) {
+    return {
+      scope: {
+        tolerance: '=',
+        offset: '=',
+        classes: '=',
+        scroller: '@'
+      },
+      link: function ($scope, $element) {
+        var options = {};
+        var opts = HeadroomService.options;
+        for (var prop in opts) {
+          options[prop] = $scope[prop] || opts[prop];
+        }
+        if ($scope.scroller) {
+          options.scroller = document.querySelector($scope.scroller);
+        }
+        var headroom = new HeadroomService($element[0], options).init();
+        $scope.$on('$destroy', function(){
+          headroom.destroy();
+        });
+      }
+    };
+  }
+
+  headroom.$inject = ['HeadroomService'];
+
+  function HeadroomService() {
+    return Headroom;
+  }
+
+  angular
+    .module('headroom', [])
+    .directive('headroom', headroom)
+    .factory('HeadroomService', HeadroomService);
+
+})(window.angular, window.Headroom);
+
+(function($) {
+
+  if(!$) {
+    return;
+  }
+
+  ////////////
+  // Plugin //
+  ////////////
+
+  $.fn.headroom = function(option) {
+    return this.each(function() {
+      var $this   = $(this),
+        data      = $this.data('headroom'),
+        options   = typeof option === 'object' && option;
+
+      options = $.extend(true, {}, Headroom.options, options);
+
+      if (!data) {
+        data = new Headroom(this, options);
+        data.init();
+        $this.data('headroom', data);
+      }
+      if (typeof option === 'string') {
+        data[option]();
+
+        if(option === 'destroy'){
+          $this.removeData('headroom');
+        }
+      }
+    });
+  };
+
+  //////////////
+  // Data API //
+  //////////////
+
+  $('[data-headroom]').each(function() {
+    var $this = $(this);
+    $this.headroom($this.data());
+  });
+
+}(window.Zepto || window.jQuery));
